494468d779199f64b70ccf7d9ed8efb79ab08a12
Lucas <46798017+lucasportifolio@users.noreply.github.com>
2019-02-04 15:11:28 -0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CalculadoraCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtvalor01 = new System.Windows.Forms.TextBox();
+            this.txtvalor02 = new System.Windows.Forms.TextBox();
+            this.txtotal = new System.Windows.Forms.TextBox();
+            this.btnsoma = new System.Windows.Forms.Button();
+            this.btnsub = new System.Windows.Forms.Button();
+            this.btndiv = new System.Windows.Forms.Button();
+            this.btnmult = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora C#";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1 Valor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "2 Valor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resultado";
+            // 
+            // txtvalor01
+            // 
+            this.txtvalor01.Location = new System.Drawing.Point(70, 113);
+            this.txtvalor01.Name = "txtvalor01";
+            this.txtvalor01.Size = new System.Drawing.Size(77, 20);
+            this.txtvalor01.TabIndex = 4;
+            // 
+            // txtvalor02
+            // 
+            this.txtvalor02.Location = new System.Drawing.Point(70, 160);
+            this.txtvalor02.Name = "txtvalor02";
+            this.txtvalor02.Size = new System.Drawing.Size(77, 20);
+            this.txtvalor02.TabIndex = 5;
+            // 
+            // txtotal
+            // 
+            this.txtotal.Location = new System.Drawing.Point(70, 208);
+            this.txtotal.Name = "txtotal";
+            this.txtotal.Size = new System.Drawing.Size(77, 20);
+            this.txtotal.TabIndex = 6;
+            // 
+            // btnsoma
+            // 
+            this.btnsoma.Location = new System.Drawing.Point(27, 297);
+            this.btnsoma.Name = "btnsoma";
+            this.btnsoma.Size = new System.Drawing.Size(75, 23);
+            this.btnsoma.TabIndex = 7;
+            this.btnsoma.Text = "+";
+            this.btnsoma.UseVisualStyleBackColor = true;
+            this.btnsoma.Click += new System.EventHandler(this.btnsoma_Click);
+            // 
+            // btnsub
+            // 
+            this.btnsub.Location = new System.Drawing.Point(132, 297);
+            this.btnsub.Name = "btnsub";
+            this.btnsub.Size = new System.Drawing.Size(75, 23);
+            this.btnsub.TabIndex = 8;
+            this.btnsub.Text = "-";
+            this.btnsub.UseVisualStyleBackColor = true;
+            this.btnsub.Click += new System.EventHandler(this.btnsub_Click);
+            // 
+            // btndiv
+            // 
+            this.btndiv.Location = new System.Drawing.Point(252, 297);
+            this.btndiv.Name = "btndiv";
+            this.btndiv.Size = new System.Drawing.Size(75, 23);
+            this.btndiv.TabIndex = 9;
+            this.btndiv.Text = "/";
+            this.btndiv.UseVisualStyleBackColor = true;
+            this.btndiv.Click += new System.EventHandler(this.btndiv_Click);
+            // 
+            // btnmult
+            // 
+            this.btnmult.Location = new System.Drawing.Point(27, 355);
+            this.btnmult.Name = "btnmult";
+            this.btnmult.Size = new System.Drawing.Size(75, 23);
+            this.btnmult.TabIndex = 10;
+            this.btnmult.Text = "*";
+            this.btnmult.UseVisualStyleBackColor = true;
+            this.btnmult.Click += new System.EventHandler(this.btnmult_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(132, 355);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 11;
+            this.btnclear.Text = "C";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(252, 355);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(75, 23);
+            this.btnsair.TabIndex = 12;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(382, 450);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnmult);
+            this.Controls.Add(this.btndiv);
+            this.Controls.Add(this.btnsub);
+            this.Controls.Add(this.btnsoma);
+            this.Controls.Add(this.txtotal);
+            this.Controls.Add(this.txtvalor02);
+            this.Controls.Add(this.txtvalor01);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtvalor01;
+        private System.Windows.Forms.TextBox txtvalor02;
+        private System.Windows.Forms.TextBox txtotal;
+        private System.Windows.Forms.Button btnsoma;
+        private System.Windows.Forms.Button btnsub;
+        private System.Windows.Forms.Button btndiv;
+        private System.Windows.Forms.Button btnmult;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnsair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraCsharp
+{
+    public partial class Form1 : Form
+    {
+        double n1, n2, total;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsub_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(txtvalor01.Text);
+            n2 = Convert.ToDouble(txtvalor02.Text);
+
+            total = n1 - n2;
+
+            txtotal.Text = total.ToString();
+        }
+
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(txtvalor01.Text);
+            n2 = Convert.ToDouble(txtvalor02.Text);
+
+            total = n1 / n2;
+
+            txtotal.Text = total.ToString();
+        }
+
+        private void btnmult_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(txtvalor01.Text);
+            n2 = Convert.ToDouble(txtvalor02.Text);
+
+            total = n1 * n2;
+
+            txtotal.Text = total.ToString();
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtvalor01.Clear();
+            txtvalor02.Clear();
+            txtotal.Clear();
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsoma_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(txtvalor01.Text);
+            n2 = Convert.ToDouble(txtvalor02.Text);
+
+            total = n1 + n2;
+
+            txtotal.Text = total.ToString();
+        }
+    }
+}
